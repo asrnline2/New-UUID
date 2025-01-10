cabfe880cd11e2ff0215e6fc41ee1aefc0b223e0
Asrnline <129671689+asrnline2@users.noreply.github.com>
2025-01-10 20:39:54 +0800

    Update NewUUID.js

--- NewUUID.js
+++ NewUUID.js
@@ -1,4 +1,10 @@
-// New UUID generation function (36x36x36)
+<h4 class="text-center mb-2">New version UUID</h4>
+    <div id="newUUIDContent" class="text-gray-800 break-all font-mono text-sm"></div>
+    </div>
+</div>
+//Add your insert code
+
+// New UUID generation function
 function generateLongUUID() {
     const chars = '0123456789abcdefghijklmnopqrstuvwxyz';
     let result = '';
@@ -11,7 +17,7 @@ function generateLongUUID() {
     }
     return result;
 }
-//
+//Insert your code
 let newUUIDs = '';
 newUUIDs += generateLongUUID() + '<br>'; // Add a line break after each UUID
 document.getElementById('newUUIDContent').innerHTML = newUUIDs;
